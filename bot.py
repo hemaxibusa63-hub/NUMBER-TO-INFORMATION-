9a21ccf9bd4d003b0a3e1275afb95883d91dd906
hemaxibusa63-hub <hemaxibusa63@gmail.com>
2026-09-08 10:43:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ from telegram.ext import (
 BOT_TOKEN = "8994031713:AAEiPqhls2JZTrFmEEK-v5penqX0rsweSYk"
 
 DATA_FILES = [
-    "Ahmedabad-547600_telegram.txt",
+    "data/Ahmedabad-547600_telegram.txt",
     "data/test_records_2.txt",
 ]
 
